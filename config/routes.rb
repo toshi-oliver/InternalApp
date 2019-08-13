0ecbe13a7c557f091b8ada09f3e32ec969d10430
toshi-oliver <igpoid@gmail.com>
2019-08-14 06:36:35 +0900

    modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   namespace :admin do
-    get 'users/new'
-    get 'users/edit'
-    get 'users/show'
-    get 'users/index'
+    resources :users
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'posts#index'
