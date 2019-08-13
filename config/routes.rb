6cc5284929e8adfa7c3406f3b024558c5164cabc
toshi-oliver <igpoid@gmail.com>
2019-08-14 06:28:33 +0900

    created admin::user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'users/new'
+    get 'users/edit'
+    get 'users/show'
+    get 'users/index'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'posts#index'
 end
