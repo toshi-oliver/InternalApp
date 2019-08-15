43eb16e49d046add9e5c39d81ff2b1cdcaa7442f
toshi-oliver <igpoid@gmail.com>
2019-08-16 01:30:06 +0900

    modified users model

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,6 @@
 class Admin::UsersController < ApplicationController
   def new
-　　@user = User.new
+　  @user = User.new
   end
 
   def create
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :name,
+    presense: true,
+    uniqueness: true,
+    format: {
+      with: /\A[a-z0-9]+\z/,
+      message: 'は小文字英数字で入力してください'
+    }
+  validates :email, presense: true, uniqueness: ture
+  validates :password,
+    length: { minimum: 8 }
 end
