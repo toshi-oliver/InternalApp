e4dca8919f8f74d85de02c31f5dc282ccbb391e0
toshi-oliver <igpoid@gmail.com>
2019-08-27 00:43:24 +0900

    add scope to post-model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,11 @@
 class Post < ApplicationRecord
   acts_as_paranoid
+  belongs_to :user
 
   validates :client_name, presence: true
   validates :email, presence: true
   validates :parking_address, presence: true, uniqueness: true
   validates :parking_present, presence: true
 
+  scope :recent, -> { order(created_at: :desc) }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :posts
 
   validates :name,
     presence: true,
@@ -8,4 +9,5 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :password,
     length: { minimum: 8 }
+    
 end
