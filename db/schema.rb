58731508589549af7399bbaa33f785b0193f041f
toshi-oliver <igpoid@gmail.com>
2019-08-29 00:17:17 +0900

    modified schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_26_060751) do
+ActiveRecord::Schema.define(version: 2019_08_28_145956) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "client_name", null: false
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(version: 2019_08_26_060751) do
     t.datetime "deleted_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "parking_present", default: true, null: false
+    t.boolean "parking_present", default: true
     t.index ["client_name"], name: "index_posts_on_client_name"
     t.index ["deleted_at"], name: "index_posts_on_deleted_at"
     t.index ["user_id"], name: "index_posts_on_user_id"
