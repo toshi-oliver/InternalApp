2212bae5a3164e33f01178e56360f4543775a179
toshi-oliver <igpoid@gmail.com>
2019-09-03 01:32:46 +0900

    add appspec.yml to deploy

---
+++ scripts/start.sh
@@ -0,0 +1,2 @@
+docker-compose -p InternalApp -f /var/www/parking-aws-documentroot/docker-compose.yml down
+docker-compose -p InternalApp -f /var/www/parking-aws-documentroot/docker-compose.yml up -d
