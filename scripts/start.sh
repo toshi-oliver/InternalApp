eca116d8deaa4757b8ccee14e15a2573259c27b7
toshi-oliver <igpoid@gmail.com>
2019-09-03 04:19:09 +0900

    modified satrt.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker start /var/www/parking-aws-documentroot/docker-compose.yml
-docker restart /var/www/parking-aws-documentroot/docker-compose.yml
+docker-compose up -d /var/www/parking-aws-documentroot/docker-compose.yml
+docker-compose restart /var/www/parking-aws-documentroot/docker-compose.yml
