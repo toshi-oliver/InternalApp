313ea0269dd0e30ade45c10b278564739ce10a26
toshi-oliver <igpoid@gmail.com>
2019-09-03 02:16:06 +0900

    modified appspec.yml to deploy

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,2 +1,2 @@
-docker-compose -f /var/www/parking-aws-documentroot/docker-compose.yml down
-docker-compose -f /var/www/parking-aws-documentroot/docker-compose.yml up -d
+docker-compose -f /var/www/parking-aws-documentroot/docker-compose.yml build
+docker-compose -f /var/www/parking-aws-documentroot/docker-compose.yml restart
